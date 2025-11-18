75198acbf8df633a72c32e9b2a5fe7aac8aadd85
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-11-18 10:46:54 +0200

    fix comments

--- src/lib/docs/paths.ts
+++ src/lib/docs/paths.ts
@@ -2,9 +2,10 @@ export function normalizePath(path: string): string {
   return path.replace(/\/$/, '')
 }
 
-// Convert an absolute content fs path to a route
-// Example: /src/content/basics/introduction/index.md -> /basics/introduction/
-//          /src/content/basics/variables/intro.md    -> /basics/variables/intro
+// Convert an absolute content fs path (from MD file) to a route
+// Examples:
+//   - /src/content/basics/introduction/index.md -> /basics/introduction/
+//   - /src/content/basics/variables/intro.md    -> /basics/variables/intro
 export function fsPathToRoute(fsPath: string): string {
   return fsPath
     .replace(/^\/src\/content/, '')
@@ -17,5 +18,8 @@ export function fsPathToRoute(fsPath: string): string {
 export function relSlug(route: string, base: string): string {
   const baseNorm = normalizePath(base)
   const routeNorm = normalizePath(route)
-  return routeNorm.replace(new RegExp('^' + baseNorm.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + '\/'), '')
+  return routeNorm.replace(
+    new RegExp('^' + baseNorm.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + '\/'),
+    '',
+  )
 }
