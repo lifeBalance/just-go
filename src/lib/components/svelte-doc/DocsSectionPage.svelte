4b3510cb86a5ac3723a69b32421f2b797f80c082
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-11-19 17:06:07 +0200

    edit message content/routing mismatch

--- src/lib/components/svelte-doc/DocsSectionPage.svelte
+++ src/lib/components/svelte-doc/DocsSectionPage.svelte
@@ -2,17 +2,21 @@
   import { createSection } from '$lib/docs/section'
   export let section: string
   export let segment: string = ''
-  export let notFound: string = 'Not found.'
+  export let contentFolderNotFound: string = `Make sure that the content folder contains a ${section} subfolder.`
 
   const { resolver } = createSection(section)
   const resolve = resolver()
   $: mod = resolve(segment)
+  console.log('mod', segment)
+  console.log('mod', section)
 </script>
 
 {#if mod}
-  <div class="prose prose-headings:uppercase prose-a:text-sd-accent prose-hr:border-sd-border prose-img:rounded-md max-w-none">
+  <div
+    class="prose prose-headings:uppercase prose-a:text-sd-accent prose-hr:border-sd-border prose-img:rounded-md max-w-none"
+  >
     <svelte:component this={(mod as any).default} />
   </div>
 {:else}
-  <p>{notFound}</p>
+  <p>⚠️ {contentFolderNotFound}</p>
 {/if}
