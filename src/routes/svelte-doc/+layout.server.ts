025d5cff031baaba1c0c271566ed1a5cf8b7d546
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-11-19 20:54:00 +0200

    and more

--- src/routes/svelte-doc/+layout.server.ts
+++
@@ -1,7 +0,0 @@
-import { createSection } from '$lib/docs/section'
-
-export const load = async ({ url }: { url: URL }) => {
-  const section = createSection('svelte-doc')
-  const { nav } = section.nav()
-  return { nav, path: url.pathname }
-}
