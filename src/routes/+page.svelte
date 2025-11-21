6f5b5cde616d311f275c7edb108755c6e1128337
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-11-21 15:18:27 +0200

    feat: add party gopher GIF and update layout in the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
   import Card from '$lib/components/svelte-doc/Card.svelte'
+  import partyGopher from '/src/assets/party-gopher.gif'
+  import dancingGopher from '/src/assets/dancing-gopher.gif'
 </script>
 
 <section class="mx-auto max-w-5xl p-6">
-  <h1 class="text-3xl font-semibold mb-6">Learn Go</h1>
+  <div class="flex justify-center items-center">
+    <img src={dancingGopher} alt="Just Go Logo" class="w-16 h-16 mr-4" />
+    <h1 class="text-3xl font-semibold">Let's get cracking!</h1>
+    <img src={partyGopher} alt="Just Go Logo" class="w-16 h-16 mr-4" />
+  </div>
   <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
     <Card href="/basics" title="Basics" description="Start with the language fundamentals." />
     <Card href="/svelte-doc" title="Svelte‑Doc" description="Build doc sites with SvelteKit helpers." />
