f5e3aa00abf636e1ddd3144283effb62702d9e9e
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-11-23 13:15:12 +0200

    refactor: update Hero component structure for improved readability

--- src/lib/components/svelte-doc/Hero.svelte
+++ src/lib/components/svelte-doc/Hero.svelte
@@ -9,16 +9,19 @@
   class="text-[clamp(2.5rem,10vw,8rem)] font-black uppercase tracking-tight leading-[0.85] text-sd-fg hero-shadow inline-block"
   style="transform: rotate({rotate});"
 >
-  <span class="block">Let's</span>
-  <span class="flex justify-between relative">
-    <span class="mr-2">get</span>
-    <img
-      src={partyGopher}
-      alt="Just Go Logo"
-      class="w-16 h-16 mr-4 absolute -right-8 -bottom-3"
-    />
+  <p>Let's</p>
+  <p>get</p>
+  <span>
+    <span>cracking</span>
+    <span class="relative inline-block">
+      !
+      <img
+        src={partyGopher}
+        alt="Just Go Logo"
+        class="w-16 absolute top-0 left-1/2 -translate-x-1/2 -translate-y-[65%]"
+      />
+    </span>
   </span>
-  <span class="block">cracking!</span>
 </svelte:element>
 
 <style>
