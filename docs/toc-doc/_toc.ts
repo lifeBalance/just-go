ed1b8253463363b7ddabec0e6513228dc94c7240
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-11-26 12:09:57 +0200

    add admonition

--- docs/toc-doc/_toc.ts
+++ docs/toc-doc/_toc.ts
@@ -1,4 +1,5 @@
 export default [
   { path: 'intro', label: 'Introduction' },
   { path: 'api', label: 'API' },
+  { path: 'ui-elements/', label: 'UI Elements' },
 ]
---
+++ docs/toc-doc/ui-elements/_toc.ts
@@ -0,0 +1,3 @@
+export default [
+  { path: 'admonition-demo', label: 'Admonition' },
+]
