de7e355950ac59532298c388794d128c1d1eeea8
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-11-28 12:56:49 +0200

    add some notes

--- docs/basics/variables/_toc.ts
+++ docs/basics/variables/_toc.ts
@@ -1,4 +1,4 @@
 export default [
-  { path: 'intro', label: 'Intro to Vars' },
-  { path: 'scope', label: 'Scope', hidden: true },
+  { path: 'intro', label: 'Introduction' },
+  { path: 'scope', label: 'Scope' },
 ]
