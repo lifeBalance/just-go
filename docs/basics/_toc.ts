edb5c9744c6a92fe6518d65adb33fcfb42f8c140
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-12-11 13:26:12 +0200

    add a bunch of notes

--- docs/basics/_toc.ts
+++ docs/basics/_toc.ts
@@ -2,5 +2,8 @@ export default [
   { path: 'introduction/', label: 'Introduction' },
   { path: 'variables/', label: 'Variables' },
   { path: 'types/', label: 'Types' },
+  { path: 'expressions/', label: 'Expressions' },
   { path: 'statements/', label: 'Statements' },
+  { path: 'composition/', label: 'Composition' },
+  { path: 'concurrency/', label: 'Concurrency' },
 ]
--- docs/basics/statements/_toc.ts
+++ docs/basics/statements/_toc.ts
@@ -2,4 +2,5 @@ export default [
   { path: 'intro', label: 'Introduction' },
   { path: 'if-statements', label: 'If Statements' },
   { path: 'switch-statements', label: 'Switch Statements' },
+  { path: 'for-loops', label: 'For Loop' },
 ]
