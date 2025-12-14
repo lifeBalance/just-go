7765f4dbdbfdd4f6b50df777ae65ba804e019907
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-12-14 23:04:20 +0200

    add more notes

--- docs/basics/statements/_toc.ts
+++ docs/basics/statements/_toc.ts
@@ -3,4 +3,5 @@ export default [
   { path: 'if-statements', label: 'If Statements' },
   { path: 'switch-statements', label: 'Switch Statements' },
   { path: 'for-loops', label: 'For Loop' },
+  { path: 'select-statements', label: 'Select' },
 ]
--- docs/basics/types/code/slices/slices10/main.go
+++ docs/basics/types/code/slices/slices10/main.go
@@ -5,11 +5,11 @@ import "fmt"
 func main() {
 	a := [...]int{1, 2, 3}
 
-	s1 := a[:] // Slicing an array
-	s1[0], s1[1] = 100, 300
-	fmt.Println(a) // [100 300 5 7 9]
+	s1 := a[:]              // Slicing an array
+	s1[0], s1[1] = 100, 300 // ✅ Changing the 1st slice
+	fmt.Println(a)          // [100 300 5 7 9] 👈 (underlying array changed)
 
-	s2 := s1[:] // Slicing a slice
-	s2[0], s2[1] = 42, 43
-	fmt.Println(a) // [42 43 5 7 9]
+	s2 := s1[:]           // Slicing a slice
+	s2[0], s2[1] = 42, 43 // ✅ Changing the 2nd slice
+	fmt.Println(a)        // [42 43 5 7 9]  👈 (underlying array changed again)
 }
