dc6564c771410fd14ef264d82fd2713c59ec8541
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-12-15 17:59:38 +0200

    edit operands

--- docs/basics/expressions/_toc.ts
+++ docs/basics/expressions/_toc.ts
@@ -2,4 +2,10 @@ export default [
   { path: 'intro', label: 'Introduction' },
   { path: 'operands', label: 'Operands' },
   { path: 'primary-expressions', label: 'Primary Expressions' },
+  { path: 'operators', label: 'Operators' },
+  { path: 'arithmetic-expressions', label: 'Arithmetic Expressions' },
+  { path: 'comparison-expressions', label: 'Comparison Expressions' },
+  { path: 'boolean-expressions', label: 'Boolean Expressions' },
+  { path: 'bitwise-and-shifts', label: 'Bitwise & Shifts' },
+  { path: 'operator-precedence', label: 'Operator Precedence' },
 ]
