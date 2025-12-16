57ae56cc2ae38c9128b07c4ada512609a55217ec
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-12-16 13:34:03 +0200

    refactor

--- docs/basics/expressions/_toc.ts
+++ docs/basics/expressions/_toc.ts
@@ -2,7 +2,6 @@ export default [
   { path: 'intro', label: 'Introduction' },
   { path: 'operands', label: 'Operands' },
   { path: 'primary-expressions', label: 'Primary Expressions' },
-  { path: 'operators', label: 'Operators' },
   { path: 'arithmetic-expressions', label: 'Arithmetic Expressions' },
   { path: 'comparison-expressions', label: 'Comparison Expressions' },
   { path: 'boolean-expressions', label: 'Boolean Expressions' },
