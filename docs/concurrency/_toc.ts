8220c7b1df2b0f4b147a5965e059ec317a9c2edf
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2026-01-21 19:09:32 +0200

    edit conc.

--- docs/concurrency/_toc.ts
+++ docs/concurrency/_toc.ts
@@ -1,4 +1,7 @@
 export default [
   { path: 'intro', label: 'Introduction' },
+  { path: 'process', label: 'Processes' },
+  { path: 'threads', label: 'Threads' },
+  { path: 'concurrency-go', label: 'Concurrency in Go' },
   { path: 'select-patterns', label: 'Select Patterns' },
 ]
