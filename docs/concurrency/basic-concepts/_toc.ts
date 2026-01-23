54a34d0fc8f3f1db7a44b60263d4467da0b3f0b4
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2026-01-23 17:29:22 +0200

    edits

--- docs/concurrency/basic-concepts/_toc.ts
+++ docs/concurrency/basic-concepts/_toc.ts
@@ -1,5 +1,7 @@
 export default [
   { path: 'process', label: 'Processes' },
   { path: 'threads', label: 'Threads' },
+  { path: 'scheduling', label: 'Scheduling' },
+  { path: 'ctx-switching', label: 'Context Switching' },
   { path: 'threading-go', label: "Go's Threading Model" },
 ]
--- docs/concurrency/scheduling/_toc.ts
+++ docs/concurrency/scheduling/_toc.ts
@@ -1,5 +1,4 @@
 export default [
   { path: 'overview/', label: 'Overview' },
   { path: 'scheduler/', label: 'Scheduler' },
-  { path: 'ctx-switching/', label: 'Context Switching' },
 ]
