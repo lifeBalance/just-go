e1f596ffa01cac4ad53c99c01256821ee16eccc0
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2026-02-05 22:51:56 +0200

    add rwmutex

--- docs/concurrency/synchronization/_toc.ts
+++ docs/concurrency/synchronization/_toc.ts
@@ -2,6 +2,7 @@ export default [
   { path: 'overview', label: 'Overview' },
   { path: 'sync-go', label: 'Syncronization in Go' },
   { path: 'mutexes', label: 'Mutexes' },
+  { path: 'rwmutexes', label: 'Readers-Writer Mutexes' },
   { path: 'channels', label: 'Channels' },
   { path: 'waitgroups', label: 'Wait Groups' },
 ]
---
+++ docs/concurrency/synchronization/code/rwmutex/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+const (
+	numReaders          = 100
+	numWriters          = 1
+	readsPerReader      = 1000
+	writesPerWriter     = 100
+	singleWriteDuration = 1 * time.Millisecond
+	singleReadDuration  = 50 * time.Microsecond
+)
+
+type Data struct {
+	data        string
+	totalWrites int
+}
+
+func main() {
+	mutex := sync.RWMutex{}
+
+	data := Data{data: "initial data", totalWrites: 0}
+	start := time.Now()
+
+	var wg sync.WaitGroup
+
+	// Writers
+	for range numWriters {
+		for range writesPerWriter {
+			wg.Go(func() {
+				writer(&data, &mutex)
+			})
+		}
+	}
+
+	// Readers
+	for range numReaders {
+		for range readsPerReader {
+			wg.Go(func() {
+				reader(&data, &mutex)
+			})
+		}
+	}
+
+	wg.Wait()
+	printBenchmark("RW Mutex", &data, time.Since(start))
+}
+
+func writer(data *Data, mutex *sync.RWMutex) {
+	mutex.Lock()
+	data.data = "some data"         // Update the shared data
+	data.totalWrites++              // Track that we did a write (also update)
+	time.Sleep(singleWriteDuration) // Simulating real work 😴
+	mutex.Unlock()
+}
+
+func reader(data *Data, mutex *sync.RWMutex) {
+	mutex.RLock()
+	_ = data.data                  // Read the shared data
+	time.Sleep(singleReadDuration) // Simulating real work 😴
+	mutex.RUnlock()
+}
+
+func printBenchmark(name string, data *Data, duration time.Duration) {
+	expectedWrites := numWriters * writesPerWriter
+	expectedReads := numReaders * readsPerReader
+
+	fmt.Printf("%s:\n", name)
+
+	status := ""
+	if data.totalWrites == expectedWrites {
+		status = "✅"
+	} else {
+		status = "❌"
+	}
+	fmt.Printf(
+		"  Total writes: %d (expected: %d) %s\n",
+		data.totalWrites,
+		expectedWrites,
+		status,
+	)
+
+	fmt.Printf(
+		"  Expected reads: %d ✅\n",
+		expectedReads,
+	)
+	fmt.Printf("  Time: %v\n", duration)
+}
